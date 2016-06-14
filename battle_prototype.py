58862fd0295179371bb418c16ba7480e42362970
Jon Schneider <jschneider2013@gmail.com>
2016-06-13 22:32:02 -0400

    battle_prototype.py first commit + wordlists

---
+++ battle_prototype.py
@@ -0,0 +1,78 @@
+import random
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LETTER_FREQ = [.08167, .01492, .02782, .04253, .12702, .02228, .02015, .06094, .06966, .00153, .00772, .04025, .02406, .06749, .07507, .01929, .00095, .05987, .06327, .09056, .02758, .00978, .02361, .00150, .01974, .00074]
+POOL_SIZE = 10
+
+def weighted_choice(choices):
+   total = sum(w for c, w in choices)
+   r = random.uniform(0, total)
+   upto = 0
+   for c, w in choices:
+      if upto + w >= r:
+         return c
+      upto += w
+   assert False, "Shouldn't get here"
+
+def generate_pool():
+    choices = zip(ALPHABET, LETTER_FREQ)
+    return [weighted_choice(choices) for _ in xrange(POOL_SIZE)]
+
+def generate_enemy():
+    return Enemy()
+
+def get_damage(attack):
+    return 1
+
+class Enemy:
+    def __init__(self):
+        self.HP = 10
+        self.name = 'standard_enemy'
+        self.attack_damage = 1
+        self.alive = True
+
+    def take_damage(self, damage):
+        self.HP -= damage
+        if self.HP <=0:
+            self.die()
+
+    def die(self):
+        print 'I died'
+        self.alive = False
+
+    def attack(self):
+        return self.attack_damage
+
+    def __str__(self):
+        return '['+self.name + ' ' + str(self.HP) + ']'
+
+def main():
+    pool = generate_pool()
+    enemy = generate_enemy()
+    enemies = [enemy]
+    dead_enemies = []
+    turn = 0
+    while(True):
+        turn += 1
+        print pool
+        for enemy in enemies:
+            print enemy
+        attack = raw_input('Type a word from the pool: ')
+
+        damage = get_damage(attack)
+
+        for enemy in enemies:
+            enemy.take_damage(damage)
+            if not enemy.alive:
+                dead_enemies.append(enemy)
+        for dead in dead_enemies:
+            enemies.remove(enemy) # does this work
+        dead_enemies = []
+
+        if turn % 10 == 0:
+            enemies.append(generate_enemy())
+
+    print ('You won!')
+
+
+main()
