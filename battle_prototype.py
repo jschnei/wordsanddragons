8b48b6dd6c50e40a089a18ad8e40083c141ad070
Anderson Wang <anderson.wang1994@gmail.com>
2016-06-14 20:25:27 -0400

    changing to sowpods word list

--- battle_prototype.py
+++ battle_prototype.py
@@ -4,7 +4,7 @@ import random
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LETTER_FREQ = [.08167, .01492, .02782, .04253, .12702, .02228, .02015, .06094, .06966, .00153, .00772, .04025, .02406, .06749, .07507, .01929, .00095, .05987, .06327, .09056, .02758, .00978, .02361, .00150, .01974, .00074]
 POOL_SIZE = 10
-WORDLIST_FILE = 'cleanbest.txt'
+WORDLIST_FILE = 'sowpods.txt'
 
 # generate wordlist
 wordlist = {line.strip() for line in open(WORDLIST_FILE, 'r')}
