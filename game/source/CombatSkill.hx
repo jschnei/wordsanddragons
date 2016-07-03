660a847999263ec3e9ac37f3c1e4a0214f19cd45
Jon Schneider <jschneider2013@gmail.com>
2016-07-02 23:45:08 -0400

    combat skills

--- game/source/CombatSkill.hx
+++ game/source/CombatSkill.hx
@@ -2,15 +2,12 @@ package;
 
 class CombatSkill
 {
-    //todo: maybe have skill take more arguments?
-    var skill:CombatHandler->Void;
     var cooldown:Int;
     var maxCooldown:Int;
     var trigger:String;
 
-    public function new(skill:CombatHandler->Void, 
-                        trigger:String, 
-                        maxCooldown:Int)
+    public function new(trigger:String,
+                        maxCooldown:Int=0)
     {
         this.maxCooldown = maxCooldown;
         if (trigger=="tick")
@@ -22,8 +19,12 @@ class CombatSkill
             this.cooldown = 0;
         }
 
-        this.skill = skill;
         this.trigger = trigger;
+    }
+
+    public function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        throw "must instantiate subclass of CombatSkill";
     }
 
     public function activate(handler:CombatHandler):Void
@@ -47,7 +48,88 @@ class CombatSkill
     }
 }
 
-public function makeThrowRockSkill(trigger:String="tick", cd:Int=1200):CombatSkill
+class SkillAttackPlayer extends CombatSkill
 {
-    //var skillFunction:CombatHandler->Void = function(handler:CombatHandler) 
+    var attackDamage:Int;
+
+    public function new(trigger:String='tick',
+                        maxCooldown:Int=500,
+                        attackDamage:Int=1)
+    {
+        super(trigger, maxCooldown);
+        this.attackDamage = attackDamage;
+    }
+
+    public override function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        handler.player.takeDamage(attackDamage);
+    }
+}
+
+class SkillHealPlayer extends CombatSkill
+{
+    var healAmount:Int;
+
+    public function new(trigger:String='attack',
+                        healAmount:Int=5)
+    {
+        super(trigger);
+        this.healAmount = healAmount;
+    }
+
+    public override function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        var attackWord:String = params.attackWord;
+
+        if(params.attackWord.length>0 && params.attackWord[0]=='H')
+        {
+            handler.player.heal(healAmount);
+        }
+    }
+}
+
+class SkillRecycle extends CombatSkill
+{
+    var numLetters:Int;
+
+    public function new(trigger:String='skillbar',
+                        maxCooldown:Int=1000,
+                        numLetters:Int=3)
+    {
+        super(trigger, maxCooldown);
+        this.numLetters = numLetters;
+    }
+
+    public override function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        handler.pool = generatePool(handler.pool.slice(numLetters));
+    }
+}
+
+class SkillSpawnEnemy extends CombatSkill
+{
+    public function new(trigger:String='tick',
+                        maxCooldown:Int=500)
+    {
+        super(trigger, maxCooldown);
+    }
+
+    public override function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        handler.enemies.push(new CombatEnemy.TriangleEnemy());
+    }
+}
+
+class SkillThrowRock extends CombatSkill
+{
+    public function new(trigger:String='tick',
+                        maxCooldown:Int=3000)
+    {
+        super(trigger, maxCooldown);
+    }
+
+    public override function skill(handler:CombatHandler, ?params:Dynamic):Void
+    {
+        handler.projectiles.push(new CombatProjectile.RockProjectile());
+    }
 }
